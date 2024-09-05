a1190c37f5cb1b982392229a913ae1c8ce583d5a
rikarani <46773591+rikarani@users.noreply.github.com>
2024-09-05 14:00:33 +0700

    Menambah Hitung Luas Persegi Panjang

--- phasya.py
+++ phasya.py
@@ -1,3 +1,4 @@
+# Hitung Luas Segitiga
 def luas_segitiga():
     a = int(input("Masukkan alas segitiga : "))
     t = int(input("Masukkan tinggi segitiga : "))
@@ -7,3 +8,14 @@ def luas_segitiga():
     print("Luas segitiga adalah : ", luas)
 
 luas_segitiga()
+
+# Hitung Luas Persegi Panjang
+def luas_persegi_panjang():
+    p = int(input("Masukkan panjang persegi : "))
+    l = int(input("Masukkan lebar persegi : "))
+
+    luas = p * l
+
+    print("Luas persegi adalah : ", luas)
+
+luas_persegi_panjang()
